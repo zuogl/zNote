545721955851ccef656481ae136a70a56d906838
zuogl <zuogl448@163.com>
2023-02-07 06:40:17 +0800

    effectFn嵌套

--- myCode/proxy.js
+++ myCode/proxy.js
@@ -168,8 +168,9 @@ const bucket = new WeakMap();
 const data = { ok: true, text: 'hello world' }
 let activeEffect;
 function effect(fn) {
-    debugger
+
     const effectFn = () => {
+        debugger
         // 调用cleanup函数完成清除工作
         cleanup(effectFn)
         // 但effectFn执行时，将其设置为当前激活的副作用函数
@@ -182,7 +183,7 @@ function effect(fn) {
     effectFn()
 
 }
-function cleanup(effectFn){
+function cleanup(effectFn) {
     for (let i = 0; i < effectFn.deps.length; i++) {
         const deps = effectFn.deps[i] // deps 是依赖集合
         deps.delete(effectFn) // 将effectFn从依赖集合中移除
@@ -197,7 +198,7 @@ const obj = new Proxy(data, {
         return target[key]
     },
     set(target, key, value) {
-
+        debugger
         target[key] = value
         trigger(target, key)
     }
@@ -223,7 +224,7 @@ function trigger(target, key) {
     if (!depsMap) return
     const effects = depsMap.get(key)
     const effectsToRun = new Set(effects)
-    effectsToRun.forEach(effectFn=>effectFn())
+    effectsToRun.forEach(effectFn => effectFn())
 
     // effects && effects.forEach(fn => fn()) //引入cleanup后，仍旧这么写，会导致无限循环，原因是因为用forEach来遍历Set时，如果一个值已经被访问过，但该值被删除并重新添加到集合，如果此时forEach没有结束，那么该值会被重新访问。
 }
@@ -232,4 +233,86 @@ function trigger(target, key) {
 effect(function effectFn() {
     console.log('副作用函数执行啦')
     document.body.innerText = obj.ok ? obj.text : 'not'
+})
+
+
+
+//  **********************************嵌套得effect与effect栈*********************************
+// 为什么effect函数要设计成可嵌套的，因为副作用函数是有肯能发生嵌套的，举例来说Vuejs中的渲染函数就是在effect中执行的。每一个组件都有自己的副作用函数，而组件是经常嵌套的。
+
+
+const bucket = new WeakMap();
+
+const data = { foo:true,bar:true }
+let activeEffect;
+function effect(fn) {
+
+    const effectFn = () => {
+        debugger
+        // 调用cleanup函数完成清除工作
+        cleanup(effectFn)
+        // 但effectFn执行时，将其设置为当前激活的副作用函数
+        activeEffect = effectFn;
+        fn()
+    }
+    // activeEffect.deps用来存储所有与该副作用函数相关联的依赖集合
+    effectFn.deps = []
+    // 执行副作用函数
+    effectFn()
+
+}
+function cleanup(effectFn) {
+    for (let i = 0; i < effectFn.deps.length; i++) {
+        const deps = effectFn.deps[i] // deps 是依赖集合
+        deps.delete(effectFn) // 将effectFn从依赖集合中移除
+    }
+    effectFn.deps.length = 0
+}
+
+const obj = new Proxy(data, {
+    get(target, key) {
+        debugger
+        track(target, key)
+        return target[key]
+    },
+    set(target, key, value) {
+        debugger
+        target[key] = value
+        trigger(target, key)
+    }
+})
+
+function track(target, key) {
+    if (!activeEffect) return;
+    let depsMap = bucket.get(target)
+    if (!depsMap) {
+        bucket.set(target, depsMap = new Map())
+    }
+    let deps = depsMap.get(key)
+    if (!deps) {
+        depsMap.set(key, deps = new Set())
+    }
+    deps.add(activeEffect)
+    // deps 就是一个与当前副作用函数存在联系的依赖集合，将其添加到activeEffect.deps数组中
+    activeEffect.deps.push(deps)
+}
+
+function trigger(target, key) {
+    const depsMap = bucket.get(target)
+    if (!depsMap) return
+    const effects = depsMap.get(key)
+    const effectsToRun = new Set(effects)
+    effectsToRun.forEach(effectFn => effectFn())
+
+    // effects && effects.forEach(fn => fn()) //引入cleanup后，仍旧这么写，会导致无限循环，原因是因为用forEach来遍历Set时，如果一个值已经被访问过，但该值被删除并重新添加到集合，如果此时forEach没有结束，那么该值会被重新访问。
+}
+
+let temp1,temp;
+effect(function effectFn1() {
+    console.log('effectFn1执行')
+    effect(function effectFn2(){
+        console.log('effectFn2执行')
+        temp2 = obj.bar
+    })
+   temp1 = obj.foo
 })
