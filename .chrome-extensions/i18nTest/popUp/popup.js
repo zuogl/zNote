78a5ea620a4eb62340016d23ce44a092a278cf6d
zuogl <zuogl448@163.com>
2023-06-02 17:51:27 +0800

    国际化设置完成

--- .chrome-extensions/i18nTest/popUp/popup.js
+++ .chrome-extensions/i18nTest/popUp/popup.js
@@ -1,37 +1,172 @@
-let textArray = ['单击我测试detectLanguage', '点击我测试getAcceptLanguages', '点击我测试getMessage', '点击我测试getUILanguage']
+const img = chrome.runtime.getURL('images/name.png')
+let textArray = [
+    chrome.i18n.getMessage("textArray0"),
+    chrome.i18n.getMessage("textArray1"),
+    chrome.i18n.getMessage("textArray2"),
+    chrome.i18n.getMessage("textArray3"),
+]
 
+let middleBoxObj = {
+    detectLanguage: {
+        title: {
+            type: 'h5',
+            text: chrome.i18n.getMessage("middleBoxObj_detectLanguage_title_text"),
+        },
+        btn1: {
+            type: 'button',
+            text: chrome.i18n.getMessage("middleBoxObj_detectLanguage_btn1_text")
+        },
+        p1: {
+            type: 'p',
+            text: chrome.i18n.getMessage("middleBoxObj_detectLanguage_p1_text")
+        },
+        btn2: {
+            type: 'button',
+            text: chrome.i18n.getMessage("middleBoxObj_detectLanguage_btn2_text")
+        },
+        p2: {
+            type: 'p',
+            text: '你好，插件。Здравствуйте, плагин Hello, plugin'
+        },
+        btn3: {
+            type: 'button',
+            text: chrome.i18n.getMessage("middleBoxObj_detectLanguage_btn3_text")
+        },
+        p3: {
+            type: 'p',
+            text: chrome.i18n.getMessage("middleBoxObj_detectLanguage_p3_text")
+        }
+    },
+    getMessage: {
+        title: {
+            type: 'h5',
+            text: chrome.i18n.getMessage("middleBoxObj_getMessage_title_text"),
+            showBigImg: true
+        },
+        btn1: {
+            type: 'button',
+            text: chrome.i18n.getMessage("middleBoxObj_getMessage_btn1_text"),
+        },
+        p1: {
+            type: 'p',
+            text: chrome.i18n.getMessage("middleBoxObj_getMessage_p1_text"),
+        },
+        btn2: {
+            type: 'button',
+            text: chrome.i18n.getMessage("middleBoxObj_getMessage_btn2_text"),
+        },
+        p2: {
+            type: 'p',
+            text: 666,
+        },
+        btn3: {
+            type: 'button',
+            text: chrome.i18n.getMessage("middleBoxObj_getMessage_btn3_text"),
+        },
+        p3: {
+            type: 'p',
+            text: chrome.i18n.getMessage("middleBoxObj_getMessage_p3_text")
+        },
+    }
+}
 
 let btns = document.querySelectorAll(".btn-wraper button")
 let result = document.querySelector(".result")
-let middleBox = document.querySelector(".middleBox")
+let bigImg = document.querySelector('.bigImg')
+let closeBigImg = document.querySelector(".closeBigImg");
 
-let callbackList = [
-    {
-        callback: () => {
-
-            chrome.i18n.detectLanguage('1994年任MIT亚太地区（中国）联络负责人。1995年底回国任美国ISI公司驻中国首席代表。 1996年在MIT媒体实验室主任尼葛洛庞帝教授和MIT斯隆商学院爱德华·罗伯特教授的风险投资支持下创建了爱特信公司，成为中国第一家以风险投资资金建立的互联网公司。1998年2月25日，爱特信正式推出搜狐产品，并更名为搜狐公司。2000年7月12日，在美国纳斯达克成功挂牌上市（NASDAQ:SOHU）。 2002年7月17日，搜狐打破中国互联网的僵局，实现赢利。同年10月17日，作为评委嘉宾出席2002福特环保奖颁奖。2003年，在上市公司中国科技人物财富排行榜上他次于丁磊获得亚军。在胡润制造的2003年中国IT富豪五十强中张朝阳亦名列三甲。同年3月6日，经中华全国工商业联合会第九次会员代表大会选举，当选中华全国工商业联合会第九届执行委员会委员。', (res) => {
-                result.innerHTML = ''
-                let r = JSON.stringify(res,null,2);
-                result.innerHTML = r;
+closeBigImg.innerText = chrome.i18n.getMessage("closeBigImg"),
+    closeBigImg.addEventListener('click', () => {
+        bigImg.classList.remove("show")
+        closeBigImg.classList.remove("show")
+    })
+function middleBoxRender(rmOrAdd, detOrMess = 'det', obj = {}) {
+    let pNode = middleBoxShowControl(rmOrAdd)
+    if (!pNode) return
+    result.innerHTML = ''
+    pNode.innerHTML = ''
+    let keys = Object.keys(obj);
+    for (let i = 0; i < keys.length; i++) {
+        let dom = document.createElement(obj[keys[i]].type)
+        dom.innerHTML = obj[keys[i]].text
+        if (obj[keys[i]].type === 'button') {
+            dom.dataset.text = obj[keys[i + 1]].text
+            dom.addEventListener('click', (e) => {
+                let text = e.target.dataset.text
+                detOrMess === 'det' ? detectLanguage(text) : getMessage(text)
             })
+        }
+        pNode.appendChild(dom)
+    }
+    document.querySelector(".middleBox h5").addEventListener('click', () => {
+        bigImg.src = img;
+        bigImg.classList.add('show')
+        closeBigImg.classList.add('show')
+    })
+}
 
-        }
-    },
-    {
-        callback: (e) => {
-            console.log('e', e)
-        }
-    },
-    {
-        callback: (e) => {
-            console.log('e', e)
-        }
-    },
-    {
-        callback: (e) => {
-            console.log('e', e)
-        }
-    },
+function middleBoxShowControl(rmOrAdd) {
+    if (!rmOrAdd) {
+        let middleBox = document.querySelector(".middleBox")
+        middleBox && document.body.removeChild(middleBox);
+        return false
+    }
+    if (rmOrAdd) {
+        let div = document.querySelector(".middleBox") || document.createElement("div")
+        div.classList.add('middleBox')
+        document.body.insertBefore(div, result)
+        return document.querySelector(".middleBox")
+    }
+}
+
+function resultRender(res, message = null) {
+    result.innerHTML = ''
+    if (message) {
+        let p = document.createElement('p');
+        p.innerText = message;
+        result.appendChild(p)
+    }
+    let pre = document.createElement('pre')
+    let r = JSON.stringify(res, null, 2);
+    pre.innerHTML = r;
+    result.appendChild(pre)
+
+}
+function detectLanguage(text) {
+    chrome.i18n.detectLanguage(text, res => resultRender(res))
+}
+
+function getAcceptLanguages() {
+    middleBoxRender(false)
+    var message = chrome.i18n.getMessage("getAcceptLanguagesMessage");
+    chrome.i18n.getAcceptLanguages((res) => resultRender(res, message))
+}
+function getUILanguage() {
+    middleBoxRender(false)
+    let res = chrome.i18n.getUILanguage()
+    resultRender(res)
+}
+
+function getMessage(parames) {
+    var res;
+    if (parames === 'test') {
+        res = chrome.i18n.getMessage('test')
+    } else if (parames === '666') {
+        res = chrome.i18n.getMessage(666)
+    } else {
+        res = chrome.i18n.getMessage('name', [
+            chrome.i18n.getMessage("middleBoxObj_getMessage_p3_0_text"),
+            chrome.i18n.getMessage("middleBoxObj_getMessage_p3_1_text"),
+            chrome.i18n.getMessage("middleBoxObj_getMessage_p3_2_text")
+        ])
+    }
+    resultRender(res)
+}
+let callbackList = [
+    { callback: () => middleBoxRender(true, 'det', middleBoxObj.detectLanguage) },
+    { callback: () => getAcceptLanguages() },
+    { callback: () => middleBoxRender(true, 'mess', middleBoxObj.getMessage) },
+    { callback: () => getUILanguage() },
 ]
 
 btns.forEach((btn, index) => {
